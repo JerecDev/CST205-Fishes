f50290408da14132c1112c08c4dff7eb8e59ad4a
Angelo Ediriweera <54722173+angeloediriweera@users.noreply.github.com>
2021-05-05 14:40:00 -0700

    Add files via upload

---
+++ cst205final/fish.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, flash, redirect
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
