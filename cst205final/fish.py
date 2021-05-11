b9733f4545e845c06977a7fa28ecd5192e6b7667
ryGutierrez <ryangutierrez831@gmail.com>
2021-05-10 17:17:04 -0700

    First Commit

--- cst205final/fish.py
+++ cst205final/fish.py
@@ -5,10 +5,20 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 import json
+from random import choices
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+# Moved request to outside functions to load faster
+r = requests.get('https://www.fishwatch.gov/api/species')
+data = r.json()
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/random/<tbSize>')
+def random(tbSize):
+    randList = choices(data, k=int(tbSize))
+    return render_template('random.html', randList=randList)
