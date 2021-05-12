8bcc24f097b04bf9cf1e1bca4cb210337861efb8
JerecDev <zachabbett@gmail.com>
2021-05-12 15:41:32 -0700

    Created header
    Created a basic header to navigate between the pages we made

--- cst205final/fish.py
+++ cst205final/fish.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import requests
+import json
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -10,3 +12,13 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/fish', methods=['GET', 'post'])
+def fish():
+    req = requests.get('https://www.fishwatch.gov/api/species')
+    data = json.loads(req.content)
+    #data = req.content
+    return render_template('fish.html', data=data)
+
+   
+
