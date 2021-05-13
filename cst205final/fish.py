184c2997adab71956bf4ff0dd6aba7bde693a2db
Ryan Gutierrez <ryangutierrez831@gmail.com>
2021-05-12 20:18:40 -0700

    Fixed Merge conflicts and merged random-fish-list with patch-1

--- cst205final/fish.py
+++ cst205final/fish.py
@@ -5,10 +5,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 import json
-<<<<<<< HEAD
-=======
 from random import choices
->>>>>>> random-fish-list
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -21,20 +18,9 @@ data = r.json()
 def index():
     return render_template('index.html')
 
-<<<<<<< HEAD
-@app.route('/fish', methods=['GET', 'post'])
-def fish():
-    req = requests.get('https://www.fishwatch.gov/api/species')
-    data = json.loads(req.content)
-    #data = req.content
-    return render_template('fish.html', data=data)
-
-   
-
-=======
 @app.route('/fish/<tbSize>', methods=['GET', 'post'])
 def fish(tbSize):
     randList = choices(data, k=int(tbSize))
     #data = req.content
     return render_template('fish.html', data=data, randList=randList)
->>>>>>> random-fish-list
+    
