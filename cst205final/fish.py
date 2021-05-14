69a80d8159982c8e8a0e0c1665fd524eb924b063
ebarskyex <ebarskyex@gmail.com>
2021-05-14 13:40:27 -0700

    Added css stylings
    Added a file for css stylings and included the file in the html pages

--- cst205final/fish.py
+++ cst205final/fish.py
@@ -5,20 +5,21 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 import json
+from random import choices
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+# Moved request to outside functions to load faster
+r = requests.get('https://www.fishwatch.gov/api/species')
+data = r.json()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/fish', methods=['GET', 'post'])
-def fish():
-    req = requests.get('https://www.fishwatch.gov/api/species')
-    data = json.loads(req.content)
+@app.route('/fish/<tbSize>', methods=['GET', 'post'])
+def fish(tbSize):
+    randList = choices(data, k=int(tbSize))
     #data = req.content
-    return render_template('fish.html', data=data)
-
-   
-
+    return render_template('fish.html', data=data, randList=randList)
